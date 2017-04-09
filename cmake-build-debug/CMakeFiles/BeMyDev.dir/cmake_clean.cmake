b630b87863455d4e24077520b12cd9c2d38ad072
Mohsan BUTT <mohsan@mbp-de-mohsan.home>
2017-04-09 03:54:56 +0200

    Init project

---
+++ cmake-build-debug/CMakeFiles/BeMyDev.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BeMyDev.dir/main.cpp.o"
+  "BeMyDev.pdb"
+  "BeMyDev"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BeMyDev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BeMyDev.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BeMyDev.
+# This may be replaced when dependencies are built.
